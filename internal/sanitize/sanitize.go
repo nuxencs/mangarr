bb94ea87c5d6c49d0011c0592cf762be6beb1214
nuxen <theonenuxen@proton.me>
2024-11-19 22:20:34 +0100

    fix(naming): inconsistent punctuation

--- internal/sanitize/sanitize.go
+++ internal/sanitize/sanitize.go
@@ -5,16 +5,30 @@ import (
 	"strings"
 )
 
+var (
+	illegalCharacters = regexp.MustCompile(`[<>:"/\\|?*]`)
+	replacementMap    = map[rune]rune{
+		'’': '\'', // inconsistency in naming between MangaDex and Flame Comics
+	}
+)
+
 // Filename removes problematic characters from the chapter title
 func Filename(title string) string {
-	// Compile the regex pattern
-	r := regexp.MustCompile(`[<>:"/\\|?*]`)
-
 	// Trim spaces & dots
 	title = strings.Trim(title, " .")
 
 	// Remove illegal chars
-	title = r.ReplaceAllString(title, "")
+	title = illegalCharacters.ReplaceAllString(title, "")
 
-	return title
+	// Replace characters based on the replacement map
+	var result strings.Builder
+	for _, char := range title {
+		if replacement, ok := replacementMap[char]; ok {
+			result.WriteRune(replacement) // Use replacement if found
+		} else {
+			result.WriteRune(char) // Keep the character as is
+		}
+	}
+
+	return result.String()
 }
