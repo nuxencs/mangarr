d06321f691e18d532f0cbcc50d445da511197502
nuxen <theonenuxen@proton.me>
2024-11-20 18:00:14 +0100

    refactor(sanitize): use maps and string builder instead of regex (#10)
    * refactor(sanitize): use maps and string builder instead of regex
    
    * fix(sanitize): trim dots and spaces at end of function

--- internal/sanitize/sanitize.go
+++ internal/sanitize/sanitize.go
@@ -1,34 +1,44 @@
 package sanitize
 
 import (
-	"regexp"
 	"strings"
 )
 
 var (
-	illegalCharacters = regexp.MustCompile(`[<>:"/\\|?*]`)
-	replacementMap    = map[rune]rune{
-		'’': '\'', // inconsistency in naming between MangaDex and Flame Comics
+	illegalCharacters = map[rune]struct{}{
+		'<':  {},
+		'>':  {},
+		':':  {},
+		'/':  {},
+		'\\': {},
+		'|':  {},
+		'?':  {},
+		'*':  {},
+	}
+	replacementCharacters = map[rune]rune{
+		'’': '\'', // fix inconsistency in naming between MangaDex and Flame Comics
 	}
 )
 
-// Filename removes problematic characters from the chapter title
+// Filename removes problematic characters and replaces specified characters in filename candidates
 func Filename(title string) string {
-	// Trim spaces & dots
-	title = strings.Trim(title, " .")
-
-	// Remove illegal chars
-	title = illegalCharacters.ReplaceAllString(title, "")
-
-	// Replace characters based on the replacement map
-	var result strings.Builder
+	// Process characters
+	result := new(strings.Builder)
 	for _, char := range title {
-		if replacement, ok := replacementMap[char]; ok {
-			result.WriteRune(replacement) // Use replacement if found
+		if _, isIllegal := illegalCharacters[char]; isIllegal {
+			// Skip illegal characters
+			continue
+		}
+		if replacementCharacter, hasReplacement := replacementCharacters[char]; hasReplacement {
+			// Use replacementCharacter if found
+			result.WriteRune(replacementCharacter)
 		} else {
-			result.WriteRune(char) // Keep the character as is
+			// Keep the character as is
+			result.WriteRune(char)
 		}
 	}
 
-	return result.String()
+	sanitizedTitle := strings.Trim(result.String(), " .")
+
+	return sanitizedTitle
 }
