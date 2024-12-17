c140da7f347c3ba24141031da177218d5f738fbb
nuxen <theonenuxen@proton.me>
2024-12-17 17:11:30 +0100

    fix(source): ComicK JSON fetching

--- internal/source/comick.go
+++ internal/source/comick.go
@@ -1,12 +1,9 @@
 package source
 
 import (
-	"bufio"
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/avast/retry-go"
-	"mangarr/internal/sanitize"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -14,12 +11,17 @@ import (
 	"time"
 
 	"mangarr/internal/domain"
+	"mangarr/internal/sanitize"
 	"mangarr/internal/sharedhttp"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
 )
 
 const (
 	comickURL         = "https://api.comick.fun"
-	comickResultLimit = 500
+	comickImageHost   = "https://meo.comick.pictures"
+	comickResultLimit = 5000
 
 	// tag name for the "Long Strip" format to identify Manhwa
 	comickLongStrip = "Long Strip"
@@ -33,76 +35,9 @@ type comick struct {
 }
 
 type comickManga struct {
-	FirstChap struct {
-		Chap      string      `json:"chap"`
-		Hid       string      `json:"hid"`
-		Lang      string      `json:"lang"`
-		GroupName []string    `json:"group_name"`
-		Vol       interface{} `json:"vol"`
-	} `json:"firstChap"`
 	Comic struct {
-		Id      int    `json:"id"`
-		Hid     string `json:"hid"`
-		Title   string `json:"title"`
-		Country string `json:"country"`
-		Status  int    `json:"status"`
-		Links   struct {
-			Al  string `json:"al"`
-			Ap  string `json:"ap"`
-			Mu  string `json:"mu"`
-			Mal string `json:"mal"`
-			Raw string `json:"raw"`
-		} `json:"links"`
-		LastChapter                          int         `json:"last_chapter"`
-		ChapterCount                         int         `json:"chapter_count"`
-		Demographic                          int         `json:"demographic"`
-		UserFollowCount                      int         `json:"user_follow_count"`
-		FollowRank                           int         `json:"follow_rank"`
-		FollowCount                          int         `json:"follow_count"`
-		Desc                                 string      `json:"desc"`
-		Parsed                               string      `json:"parsed"`
-		Slug                                 string      `json:"slug"`
-		Mismatch                             interface{} `json:"mismatch"`
-		Year                                 int         `json:"year"`
-		BayesianRating                       interface{} `json:"bayesian_rating"`
-		RatingCount                          int         `json:"rating_count"`
-		ContentRating                        string      `json:"content_rating"`
-		TranslationCompleted                 bool        `json:"translation_completed"`
-		ChapterNumbersResetOnNewVolumeManual bool        `json:"chapter_numbers_reset_on_new_volume_manual"`
-		FinalChapter                         interface{} `json:"final_chapter"`
-		FinalVolume                          interface{} `json:"final_volume"`
-		Noindex                              bool        `json:"noindex"`
-		Adsense                              bool        `json:"adsense"`
-		LoginRequired                        bool        `json:"login_required"`
-		Recommendations                      []struct {
-			Up      int `json:"up"`
-			Down    int `json:"down"`
-			Total   int `json:"total"`
-			Relates struct {
-				Title    string `json:"title"`
-				Slug     string `json:"slug"`
-				Hid      string `json:"hid"`
-				MdCovers []struct {
-					Vol   string `json:"vol"`
-					W     int    `json:"w"`
-					H     int    `json:"h"`
-					B2Key string `json:"b2key"`
-				} `json:"md_covers"`
-			} `json:"relates"`
-		} `json:"recommendations"`
-		RelateFrom []struct {
-			RelateTo struct {
-				Slug  string `json:"slug"`
-				Title string `json:"title"`
-			} `json:"relate_to"`
-			MdRelates struct {
-				Name string `json:"name"`
-			} `json:"md_relates"`
-		} `json:"relate_from"`
-		MdTitles []struct {
-			Title string `json:"title"`
-			Lang  string `json:"lang"`
-		} `json:"md_titles"`
+		Hid             string `json:"hid"`
+		Title           string `json:"title"`
 		MdComicMdGenres []struct {
 			MdGenres struct {
 				Name  string  `json:"name"`
@@ -111,48 +46,9 @@ type comickManga struct {
 				Group string  `json:"group"`
 			} `json:"md_genres"`
 		} `json:"md_comic_md_genres"`
-		MdCovers []struct {
-			Vol   string `json:"vol"`
-			W     int    `json:"w"`
-			H     int    `json:"h"`
-			B2Key string `json:"b2key"`
-		} `json:"md_covers"`
-		MuComics struct {
-			MuComicPublishers []struct {
-				MuPublishers struct {
-					Title string `json:"title"`
-					Slug  string `json:"slug"`
-				} `json:"mu_publishers"`
-			} `json:"mu_comic_publishers"`
-			LicensedInEnglish interface{} `json:"licensed_in_english"`
-			MuComicCategories []struct {
-				MuCategories struct {
-					Title string `json:"title"`
-					Slug  string `json:"slug"`
-				} `json:"mu_categories"`
-				PositiveVote int `json:"positive_vote"`
-				NegativeVote int `json:"negative_vote"`
-			} `json:"mu_comic_categories"`
-		} `json:"mu_comics"`
-		Iso6391    string `json:"iso639_1"`
-		LangName   string `json:"lang_name"`
-		LangNative string `json:"lang_native"`
 	} `json:"comic"`
-	Artists []struct {
-		Name string `json:"name"`
-		Slug string `json:"slug"`
-	} `json:"artists"`
-	Authors []struct {
-		Name string `json:"name"`
-		Slug string `json:"slug"`
-	} `json:"authors"`
-	LangList       []string    `json:"langList"`
-	Recommendable  bool        `json:"recommendable"`
-	Demographic    string      `json:"demographic"`
-	EnglishLink    interface{} `json:"englishLink"`
-	MatureContent  bool        `json:"matureContent"`
-	CheckVol2Chap1 bool        `json:"checkVol2Chap1"`
 }
+
 type comickChapters struct {
 	Chapters []comickChapterData `json:"chapters"`
 	Total    int                 `json:"total"`
@@ -160,32 +56,27 @@ type comickChapters struct {
 }
 
 type comickChapterData struct {
-	Id               int        `json:"id"`
 	Chap             string     `json:"chap"`
 	Title            *string    `json:"title"`
 	Vol              *string    `json:"vol"`
-	Lang             string     `json:"lang"`
-	CreatedAt        time.Time  `json:"created_at"`
-	UpdatedAt        time.Time  `json:"updated_at"`
-	UpCount          int        `json:"up_count"`
-	DownCount        int        `json:"down_count"`
-	IsTheLastChapter bool       `json:"is_the_last_chapter"`
 	PublishAt        *time.Time `json:"publish_at"`
 	GroupName        []string   `json:"group_name"`
 	Hid              string     `json:"hid"`
-	Identities       *struct {
-		Id     string `json:"id"`
-		Traits struct {
-			Username string `json:"username"`
-			Gravatar string `json:"gravatar"`
-		} `json:"traits"`
-	} `json:"identities"`
 	MdChaptersGroups []struct {
 		MdGroups struct {
 			Title string `json:"title"`
 			Slug  string `json:"slug"`
 		} `json:"md_groups"`
 	} `json:"md_chapters_groups"`
+}
+
+type comickImageData []struct {
+	H         int    `json:"h"`
+	W         int    `json:"w"`
+	Name      string `json:"name"`
+	S         int    `json:"s"`
+	B2Key     string `json:"b2key"`
+	Optimized int    `json:"optimized"`
 }
 
 func NewComick(mangaURL, group, language string) domain.Source {
@@ -218,7 +109,7 @@ func (c *comick) ValidateInput() error {
 	return nil
 }
 
-func (c *comick) GetManga(ctx context.Context) (domain.Manga, error) {
+func (c *comick) GetManga(_ context.Context) (domain.Manga, error) {
 	var mangaResp comickManga
 	var isManhwa bool
 
@@ -232,45 +123,14 @@ func (c *comick) GetManga(ctx context.Context) (domain.Manga, error) {
 		return domain.Manga{}, fmt.Errorf("failed to build URL: %w", err)
 	}
 
-	u, err := url.Parse(path)
+	jsonResp, err := c.fetchJSON(path)
 	if err != nil {
-		return domain.Manga{}, fmt.Errorf("failed to parse URL %s: %w", path, err)
+		return domain.Manga{}, fmt.Errorf("failed to fetch json from URL %s: %w", path, err)
 	}
 
-	params := url.Values{
-		"tachiyomi": []string{"true"},
-	}
-
-	u.RawQuery = params.Encode()
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	err = json.NewDecoder(strings.NewReader(jsonResp)).Decode(&mangaResp)
 	if err != nil {
-		return domain.Manga{}, fmt.Errorf("failed to create request: %w", err)
-	}
-
-	req.Header.Set("User-Agent", "mangarr")
-
-	retryErr := retry.Do(func() error {
-		resp, err := sharedhttp.ExecRequest(*c.Client, req)
-		if err != nil {
-			return fmt.Errorf("failed to execute request %s: %w", req.URL, err)
-		}
-
-		buf := bufio.NewReader(resp.Body)
-
-		err = json.NewDecoder(buf).Decode(&mangaResp)
-		if err != nil {
-			return retry.Unrecoverable(fmt.Errorf("failed to decode response: %w", err))
-		}
-
-		return nil
-	},
-		retry.Delay(time.Second*3),
-		retry.Attempts(3),
-		retry.MaxJitter(time.Second*1),
-	)
-	if retryErr != nil {
-		return domain.Manga{}, fmt.Errorf("failed to execute request %s: %w", req.URL, retryErr)
+		return domain.Manga{}, fmt.Errorf("failed to decode response: %w", err)
 	}
 
 	id := mangaResp.Comic.Hid
@@ -298,7 +158,7 @@ func (c *comick) GetManga(ctx context.Context) (domain.Manga, error) {
 	}, nil
 }
 
-func (c *comick) GetChapters(ctx context.Context, manga domain.Manga) error {
+func (c *comick) GetChapters(_ context.Context, manga domain.Manga) error {
 	var chapterResp comickChapters
 
 	path, err := url.JoinPath(comickURL, "comic", manga.ID, "chapters")
@@ -312,41 +172,20 @@ func (c *comick) GetChapters(ctx context.Context, manga domain.Manga) error {
 	}
 
 	params := url.Values{
-		"lang":      []string{c.Language},
-		"limit":     []string{fmt.Sprintf("%d", comickResultLimit)},
-		"tachiyomi": []string{"true"},
+		"lang":  []string{c.Language},
+		"limit": []string{fmt.Sprintf("%d", comickResultLimit)},
 	}
 
 	u.RawQuery = params.Encode()
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	jsonResp, err := c.fetchJSON(u.String())
 	if err != nil {
-		return fmt.Errorf("failed to create request: %w", err)
+		return fmt.Errorf("failed to fetch json from URL %s: %w", u.String(), err)
 	}
 
-	req.Header.Set("User-Agent", "mangarr")
-
-	retryErr := retry.Do(func() error {
-		resp, err := sharedhttp.ExecRequest(*c.Client, req)
-		if err != nil {
-			return fmt.Errorf("failed to execute request %s: %w", req.URL, err)
-		}
-
-		buf := bufio.NewReader(resp.Body)
-
-		err = json.NewDecoder(buf).Decode(&chapterResp)
-		if err != nil {
-			return retry.Unrecoverable(fmt.Errorf("failed to decode response: %w", err))
-		}
-
-		return nil
-	},
-		retry.Delay(time.Second*3),
-		retry.Attempts(3),
-		retry.MaxJitter(time.Second*1),
-	)
-	if retryErr != nil {
-		return fmt.Errorf("failed to execute request %s: %w", req.URL, retryErr)
+	err = json.NewDecoder(strings.NewReader(jsonResp)).Decode(&chapterResp)
+	if err != nil {
+		return fmt.Errorf("failed to decode response: %w", err)
 	}
 
 	for _, data := range chapterResp.Chapters {
@@ -374,9 +213,40 @@ func (c *comick) GetChapters(ctx context.Context, manga domain.Manga) error {
 	return nil
 }
 
-func (c *comick) GetImageURLs(ctx context.Context, chapter *domain.Chapter) error {
-	//TODO implement me
-	panic("implement me")
+func (c *comick) GetImageURLs(_ context.Context, chapter *domain.Chapter) error {
+	var imageResp comickImageData
+	var imageInfos []domain.ImageInfo
+
+	path, err := url.JoinPath(comickURL, "chapter", chapter.ID, "get_images")
+	if err != nil {
+		return fmt.Errorf("failed to build URL: %w", err)
+	}
+
+	jsonResp, err := c.fetchJSON(path)
+	if err != nil {
+		return fmt.Errorf("failed to fetch json from URL %s: %w", path, err)
+	}
+
+	err = json.NewDecoder(strings.NewReader(jsonResp)).Decode(&imageResp)
+	if err != nil {
+		return fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	for _, image := range imageResp {
+		imagePath, err := url.JoinPath(comickImageHost, image.B2Key)
+		if err != nil {
+			return fmt.Errorf("failed to build URL: %w", err)
+		}
+
+		imageInfos = append(imageInfos, domain.ImageInfo{ImageURL: imagePath})
+	}
+
+	if len(imageInfos) == 0 {
+		return fmt.Errorf("failed to get image URLs for chapter ID %s", chapter.ID)
+	}
+
+	chapter.ImageInfo = imageInfos
+	return nil
 }
 
 func (c *comick) extractSlug(urlString string) (string, error) {
@@ -395,6 +265,26 @@ func (c *comick) extractSlug(urlString string) (string, error) {
 	}
 
 	return "", fmt.Errorf("no path segment found")
+}
+
+func (c *comick) fetchJSON(path string) (string, error) {
+	launcherPath, _ := launcher.LookPath()
+	launcherURL := launcher.New().Bin(launcherPath).MustLaunch()
+	browser := rod.New().ControlURL(launcherURL).MustConnect()
+	defer browser.MustClose()
+
+	page := browser.MustPage(path).MustWaitDOMStable()
+	resp, err := page.Element("pre")
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch chapters page: %w", err)
+	}
+
+	jsonResp, err := resp.Text()
+	if err != nil {
+		return "", fmt.Errorf("failed to get json from html: %w", err)
+	}
+
+	return jsonResp, nil
 }
 
 func (c *comick) processChapter(data comickChapterData, manga *domain.Manga) error {
