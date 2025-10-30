17320d79d8e8500fcf38940c54a8d2d43ca82db3
nuxen <theonenuxen@proton.me>
2025-10-30 17:54:01 +0100

    fix(flamecomics): image url fetching

--- internal/source/flamecomics.go
+++ internal/source/flamecomics.go
@@ -19,7 +19,10 @@ import (
 	"github.com/gocolly/colly/extensions"
 )
 
-const flamecomicsCDNURL = "https://cdn.flamecomics.xyz"
+const (
+	flamecomicsURL    = "https://flamecomics.xyz"
+	flamecomicsCDNURL = "https://cdn.flamecomics.xyz"
+)
 
 type flamecomicsResponse struct {
 	Props struct {
@@ -40,21 +43,34 @@ type flamecomicsResponse struct {
 			Chapters []flamescansChapter `json:"chapters"`
 		} `json:"pageProps"`
 	} `json:"props"`
-	Query struct {
-		Id string `json:"id"`
-	} `json:"query"`
 }
 
 type flamescansChapter struct {
-	ChapterID     int                        `json:"chapter_id"`
-	SeriesID      int                        `json:"series_id"`
-	Chapter       string                     `json:"chapter"`
-	Title         string                     `json:"title"`
-	Images        map[string]flamescansImage `json:"images"`
-	Language      string                     `json:"language"`
-	ReleaseDate   int64                      `json:"release_date"`
-	Token         string                     `json:"token"`
-	UnixTimestamp int64                      `json:"unix_timestamp"`
+	ChapterID   int    `json:"chapter_id"`
+	SeriesID    int    `json:"series_id"`
+	Chapter     string `json:"chapter"`
+	Title       string `json:"title"`
+	ReleaseDate int64  `json:"release_date"`
+	Token       string `json:"token"`
+}
+
+type flamecomicsChapterResponse struct {
+	Props struct {
+		PageProps struct {
+			Chapter struct {
+				SeriesId      int                        `json:"series_id"`
+				ChapterId     int                        `json:"chapter_id"`
+				Chapter       string                     `json:"chapter"`
+				ChapterTitle  string                     `json:"chapter_title"`
+				Images        map[string]flamescansImage `json:"images"`
+				Token         string                     `json:"token"`
+				ReleaseDate   int                        `json:"release_date"`
+				EditTime      int                        `json:"edit_time"`
+				UnixTimestamp int                        `json:"unix_timestamp"`
+				Title         string                     `json:"title"`
+			} `json:"chapter"`
+		} `json:"pageProps"`
+	} `json:"props"`
 }
 
 type flamescansImage struct {
@@ -152,35 +168,15 @@ func (f *flamecomics) GetManga(_ context.Context) (domain.Manga, error) {
 
 		chapterNum := float32(chapterNumF64)
 
-		keys := make([]string, 0, len(responseChapter.Images))
-		for k := range responseChapter.Images {
-			keys = append(keys, k)
-		}
-		sort.Slice(keys, func(i, j int) bool {
-			iNum, _ := strconv.ParseFloat(keys[i], 32)
-			jNum, _ := strconv.ParseFloat(keys[j], 32)
-			return iNum < jNum
-		})
-
-		imageURLs := make([]domain.ImageInfo, 0, len(responseChapter.Images))
-		for _, key := range keys {
-			i := responseChapter.Images[key]
-
-			imageURLs = append(imageURLs, domain.ImageInfo{
-				ImageURL: fmt.Sprintf("%s/uploads/images/series/%d/%s/%s",
-					flamecomicsCDNURL,
-					responseData.Props.PageProps.Series.SeriesId,
-					responseChapter.Token,
-					i.Name,
-				),
-			})
+		path, err := url.JoinPath(flamecomicsURL, "series", fmt.Sprintf("%d", responseChapter.SeriesID), responseChapter.Token)
+		if err != nil {
+			return domain.Manga{}, fmt.Errorf("building URL: %w", err)
 		}
 
 		manga.Chapters[chapterNum] = domain.Chapter{
-			ID:        responseChapter.Token,
-			Number:    chapterNum,
-			Title:     responseChapter.Title,
-			ImageInfo: imageURLs,
+			URL:    path,
+			Number: chapterNum,
+			Title:  responseChapter.Title,
 		}
 	}
 
@@ -199,6 +195,70 @@ func (f *flamecomics) GetChapters(_ context.Context, _ domain.Manga) error {
 	return nil
 }
 
-func (f *flamecomics) GetImageURLs(_ context.Context, _ *domain.Chapter) error {
+func (f *flamecomics) GetImageURLs(_ context.Context, chapter *domain.Chapter) error {
+	var chapterResponse flamecomicsChapterResponse
+	var errors []error
+	c := f.Collector.Clone()
+
+	c.OnError(func(r *colly.Response, err error) {
+		errors = append(errors, fmt.Errorf("requesting URL %s: %w", r.Request.URL, err))
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		match := jsonRegex.FindSubmatch(r.Body)
+		if len(match) < 2 {
+			errors = append(errors, fmt.Errorf("finding chapter data in response from URL %s", r.Request.URL))
+			return
+		}
+
+		buf := bytes.NewReader(match[1])
+
+		err := json.NewDecoder(buf).Decode(&chapterResponse)
+		if err != nil {
+			errors = append(errors, fmt.Errorf("decoding chapter data from json: %w", err))
+			return
+		}
+	})
+
+	err := c.Visit(chapter.URL)
+	if err != nil {
+		return fmt.Errorf("visiting URL %s: %w", chapter.URL, err)
+	}
+
+	if len(errors) > 0 {
+		return fmt.Errorf("processing %d URLs: %w", len(errors), errors[0])
+	}
+
+	responseChapter := chapterResponse.Props.PageProps.Chapter
+
+	keys := make([]string, 0, len(responseChapter.Images))
+	for k := range responseChapter.Images {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		iNum, _ := strconv.ParseFloat(keys[i], 32)
+		jNum, _ := strconv.ParseFloat(keys[j], 32)
+		return iNum < jNum
+	})
+
+	imageURLs := make([]domain.ImageInfo, 0, len(responseChapter.Images))
+	for _, key := range keys {
+		i := responseChapter.Images[key]
+
+		imageURLs = append(imageURLs, domain.ImageInfo{
+			ImageURL: fmt.Sprintf("%s/uploads/images/series/%d/%s/%s",
+				flamecomicsCDNURL,
+				chapterResponse.Props.PageProps.Chapter.SeriesId,
+				responseChapter.Token,
+				i.Name,
+			),
+		})
+	}
+
+	if len(imageURLs) == 0 {
+		return fmt.Errorf("getting image URLs for chapter %g", chapter.Number)
+	}
+
+	chapter.ImageInfo = imageURLs
 	return nil
 }
